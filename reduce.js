cac6384055f5aa0a66800166d7dd106a0ab1aa96
Gabriel Morris <gabemorris@outlook.com>
2018-08-21 13:06:49 -0700

    Working reduce

--- reduce.js
+++ reduce.js
@@ -1,18 +1,23 @@
+const words =
+  'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 
-const words = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
-
-const reduceDecoder = function(str){
-  const wordsArray = str.split(' ');   //array of original string
-  const reducedArray = wordsArray.reduce((x='', amount, index, array) =>{ 
-      if (wordsArray[index].length === 3) {
-      x = x + ' ';
-      }
-      else {
-      x = x + array[index].charAt(array[index.length - 1]).toUpperCase();
-      }
-      console.log(x);
-      } )
+const reduceDecoder = function(str) {
+  const wordsArray = str.split(' '); //array of original string
+  const reducedArray = wordsArray.reduce((x, amount) => {
+    // console.log(`${index} is index`);
+    if (amount.length === 3) {
+      // console.log('word is 3 chars');
+      return x + ' ';
+    } else {
+      // console.log('adding last char');
+      return x + amount.charAt(amount.length - 1).toUpperCase();
+    }
+    // console.log(x);
+    // return x;
+  }, '');
   return reducedArray;
-}
-console.log(words);
+};
+// console.log(words);
 console.log(reduceDecoder(words));
+
+// noggin, oreo
